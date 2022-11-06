f1f97b08b4b6153f8284badef59185ac2df9ae15
kartikeya532001 <kartikeya532001@gmail.com>
2022-11-06 12:27:12 +0530

    Setup for frontend Done

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,19 @@
-import ".Assets/CSS/App.css";
+import "./Assets/CSS/App.css";
 import { Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Footer from "./Footer";
 
 
 function App() {
   return (
     <>
+    <Home />
       <Switch>
         
       </Switch>
+    <Footer />
     </>
+ 
   );
 }
 
